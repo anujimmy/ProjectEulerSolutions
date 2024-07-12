ceea69572ec7bb79c0545598854c45fc18d945df
anujimmy <anuannajimmy@gmail.com>
2024-07-12 14:50:42 +0200

    initial commit

--- src/ProjectEulerSolutions/Euler001.java
+++ src/ProjectEulerSolutions/Euler001.java
@@ -2,6 +2,6 @@ package ProjectEulerSolutions;
 
 public class Euler001 {
     public static void main(String[] args) {
-        System.out.println("Project Euler -> Problem 001 - Sum of all multiples of 3 or 5 under 1000");
+        System.out.println("Project Euler Problem 001 - Sum of all multiples of 3 or 5 under 1000");
     }
 }
